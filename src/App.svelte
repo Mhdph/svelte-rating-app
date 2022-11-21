c4abd974b34a12c25102ceeabbcd2c9b99e5e740
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-11-22 00:14:39 +0330

    Delete item and stats section compate

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
 
   let Feedback = [
     {
@@ -18,8 +19,18 @@
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
     },
   ];
+
+  $: count = Feedback.length;
+  $: avarage =
+    Feedback.reduce((a, { rating }) => a + rating, 0) / Feedback.length;
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    Feedback = Feedback.filter((item) => item.id != itemId);
+  };
 </script>
 
-<main>
-  <FeedbackList feedback={Feedback} />
+<main class="container">
+  <FeedbackStats {count} average={avarage} />
+  <FeedbackList feedback={Feedback} on:delete-feedback={deleteFeedback} />
 </main>
